6dd7364c35972f310653c307a2c19c51e1d2d82e
Joel Capillo <Joel Capillo>
2016-07-13 23:10:37 -0700

    clean up thru jslint

--- js/password-strength-directive.js
+++ js/password-strength-directive.js
@@ -1,75 +1,80 @@
-'use strict';
-angular.module('app.widget', [])       
-.directive('passwordStrength', function() {
-    return {
-        templateUrl: 'template/password-strength-widget.html',                
-        restrict: 'E',               
-        scope: {},
-        controller: function ($scope) {
+/*
+    Password Strength Directive
+    Copyright (c) 2007 - 2016 Joel Capillo (joecaps.com)
+    Version: 1.0.0
+*/
+/*global angular */
+/*jslint node: true */
+/*jslint regexp: true*/
+"use strict";
+angular.module('app.widget', [])
+    .directive('passwordStrength', function () {
+        /*jslint unparam: true*/
+        return {
+            templateUrl: 'template/password-strength-widget.html',
+            restrict: 'E',
+            scope: {},
+            controller: function ($scope) {
 
-            //declare password scope
-            $scope.password = {
-                status: 'Invalid',
-                value: '',
-                meterWidth: 0,
-                type: 'password',
-                rules:{
-                    isValidLength: false,
-                    hasNumber: false,
-                    hasLetter: false,
-                    noSpecialChar: false                            
-                }
-            };
-
-            //set to default values
-            this.setDefault = function(password){
-                password.rules.hasNumber = false; 
-                password.rules.hasLetter = false; 
-                password.rules.isValidLength = false;
-                password.rules.noSpecialChar = true;
-                $scope.password.meterWidth = 25;
-            };  
-
-
-            this.getMeterWidth = function(password){
-                var property_count = 0, valid_property_count = 0, property;
-                for (property in password.rules) {
-                    if (password.rules.hasOwnProperty(property)) {
-                        property_count = property_count + 1;
-                        if(password.rules[property]){
-                            valid_property_count = valid_property_count + 1;
-                        } 
+                //declare password scope
+                $scope.password = {
+                    status: 'Invalid',
+                    value: '',
+                    meterWidth: 0,
+                    type: 'password',
+                    rules: {
+                        isValidLength: false,
+                        hasNumber: false,
+                        hasLetter: false,
+                        noSpecialChar: false
                     }
-                }                        
-                return (valid_property_count/property_count)*100; //calculate percentage of passed criteria    
-            };
+                };
+
+                //set to default values
+                this.setDefault = function (password) {
+                    password.rules.hasNumber = false;
+                    password.rules.hasLetter = false;
+                    password.rules.isValidLength = false;
+                    password.rules.noSpecialChar = true;
+                    $scope.password.meterWidth = 25;
+                };
 
 
-            this.getStatus = function(password){
-                if(100 === password.meterWidth){
-                    return 'Valid';
-                }
-                return 'Invalid';
-            };
-        },
-        link: function ($scope, $element, $attrs, $ctrl) {
-            $ctrl.setDefault($scope.password); 
-            //listen to password change
-            $scope.onPasswordChange = function(password){ 
-                $ctrl.setDefault(password);
-                if(password.value){                       
-                    password.rules.hasNumber = 
-                            password.value.match(/\d/) ? true:false; 
-                    password.rules.hasLetter = 
-                            password.value.match(/[A-z]/) ? true:false; 
-                    password.rules.isValidLength = 
-                            password.value.match(/^.{6,}$/) ? true:false;
-                    password.rules.noSpecialChar = 
-                            !password.value.match(/[ /"]/) ? true:false;                            
-                } 
-                password.meterWidth = $ctrl.getMeterWidth(password);
-                password.status = $ctrl.getStatus(password);
-            };                    
-        }
-    };
-});
+                this.getMeterWidth = function (password) {
+                    var property_count = 0, valid_property_count = 0, property;
+                    for (property in password.rules) {
+                        if (password.rules.hasOwnProperty(property)) {
+                            property_count = property_count + 1;
+                            if (password.rules[property]) {
+                                valid_property_count = valid_property_count + 1;
+                            }
+                        }
+                    }
+                    return (valid_property_count / property_count) * 100; //calculate percentage of passed criteria    
+                };
+
+
+                this.getStatus = function (password) {
+                    if (100 === password.meterWidth) {
+                        return 'Valid';
+                    }
+                    return 'Invalid';
+                };
+            },
+            link: function ($scope, $element, $attrs, $ctrl) {
+                $ctrl.setDefault($scope.password);
+                //listen to password change
+                $scope.onPasswordChange = function (password) {
+                    $ctrl.setDefault(password);
+                    if (password.value) {
+                        password.rules.hasNumber = password.value.match(/\d/) ? true : false;
+                        password.rules.hasLetter = password.value.match(/[A-z]/) ? true : false;
+                        password.rules.isValidLength = password.value.match(/^.{6,}$/) ? true : false;
+                        password.rules.noSpecialChar = !password.value.match(/[ \/"]/) ? true : false;
+                    }
+                    password.meterWidth = $ctrl.getMeterWidth(password);
+                    password.status = $ctrl.getStatus(password);
+                };
+            }
+        };
+    });
